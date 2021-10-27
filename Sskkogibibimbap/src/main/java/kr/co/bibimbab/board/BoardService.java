e147b013db90f2541af3d601430f90a62906f05d
user <user@121.162.60.95>
2021-10-28 00:08:44 +0900

    Toy Project

--- Sskkogibibimbap/src/main/java/kr/co/bibimbab/board/BoardService.java
+++ Sskkogibibimbap/src/main/java/kr/co/bibimbab/board/BoardService.java
@@ -14,27 +14,33 @@ public class BoardService {
 	@Resource(name="BoardDao")
 	private BoardDao boardDao;
 	
+	/* 해당하는 조건의 Board List 조회 */
 	public List<BoardVO> readBoardList(String target) {
 		System.out.println("=== 서비스 들어감");
 		return boardDao.readBoardList(target);
 	}
 	
+	/* 해당하는 조건의 Board 갯수를 Count */
 	public int countBoardList(String target) {
 		return boardDao.countBoardList(target);
 	}
 	
+	/* 선택한 Board 정보를 조회 */
 	public BoardVO readBoardOne() {
 		return boardDao.readBoardOne();
 	}
 	
+	/* 입력한 Board 정보를 삽입 */
 	public int insertBoard() {
 		return boardDao.insertBoard();
 	}
 	
+	/* 입력한 Board 정보를 수정 */
 	public int updateBoard() {
 		return boardDao.updateBoard();
 	}
 
+	/* 선택한 Board 정보를 삭제 */
 	public int deleteBoard() {
 		return boardDao.deleteBoard();
 	}
